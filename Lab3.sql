50fcacca450294ea6c6466cbc99131739b2af7be
Melissa I <phoenixemere@gmail.com>
2014-09-28 22:00:23 -0400

    Melissa Iori, Lab 4, 9/28/14

--- Lab3.sql
+++ Lab3.sql
@@ -3,36 +3,58 @@
 
 --1--
 
-select name,city from agents where name='Bond';
+select name,city 
+from agents 
+where name='Bond';
 
 --2--
 
-select pid,name,quantity from products where priceusd>0.99;
+select pid,name,quantity 
+from products 
+where priceusd>0.99;
 
 --3--
 
-select ordno,qty from orders;
+select ordno,qty
+from orders;
 
 --4--
 
-select name,city from customers where city='Duluth';
+select name,city
+from customers 
+where city='Duluth';
 
 --5--
 
-select name from agents where city<>'New York' and city<>'London';
+select name
+from agents 
+where city<>'New York' 
+	and city<>'London';
 
 --6--
 
-select * from products where city<>'Dallas' and city<>'Duluth' and priceusd<=1.00;
+select *
+from products 
+where city<>'Dallas'
+	and city<>'Duluth' 
+	and priceusd<=1.00;
 
 --7--
 
-select * from orders where mon='jan' or mon='apr';
+select *
+from orders 
+where mon='jan' 
+	or mon='apr';
 
 --8--
 
-select * from orders where mon='feb' and dollars>200.00;
+select *
+from orders 
+where mon='feb' 
+	and dollars>200.00;
 
 --9--
 
-select * from orders where cid='C005';
+select *
+from orders 
+where cid='C005';
